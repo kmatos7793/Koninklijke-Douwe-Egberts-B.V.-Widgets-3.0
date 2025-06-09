e905868eb6216b5f7df50e23e1bce235036e19d5
Kenneth John Matos <kmatos@yotpo.com>
2025-06-09 18:47:20 +0800

    ESL Gaming [141068] Widgets 3.0

--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/31XbIAMPwZPxdUYcMKFC6pltDTI3fKIdq6eDgHSH" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/BF6MXqJNgaWQKSd9pxrwHE1ob8PMbEIsURlicbDn" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
