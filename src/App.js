81be38e22381fbd29253d9e59079a1525a9510c2
Kenneth John Matos <kmatos@yotpo.com>
2025-06-17 14:29:52 +0800

    Hasbro Inc Widgets 3.0

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "1145493";
+  let InstanceId_Widget = "1147046";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "";
+  let InstanceId_StarRating = "1150054";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "";
+  let ProductId = "F73075X22";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
