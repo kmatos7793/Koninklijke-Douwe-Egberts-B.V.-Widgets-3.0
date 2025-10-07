15d7a034e11eecb4742b23300b8d486dc434f641
Kenneth John Matos <kmatos@yotpo.com>
2025-10-07 16:40:12 +0800

    Koninklijke Douwe Egberts B.V. Widgets 3.0

--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/lSPVayxInT9shzOvufyDWX3YBNjN3NtYsMtjEGUM" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/5JswVXQzTVE709Upk1Nf5cS9mHyXLeyTliczkbbF" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
